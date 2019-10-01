562af2107e9f0f52235368f8a35590fba6a86306
Prem Patel <prem.patel3105@gmail.com>
2019-10-01 20:10:25 +0530

    added css to navlink

--- src/components/header.js
+++ src/components/header.js
@@ -9,57 +9,96 @@ const Content = styled.div`
 	font-size: 1.2rem;
 `;
 
+// const NavLink = styled(Link)`
+//   color: black;
+//   margin-left: 15px;
+//   text-decoration: none;
+//   display: inline-block;
+//   position: relative;
+
+//   ::after {
+//     content: "";
+//     position: absolute;
+//     width: 100%;
+//     transform: scaleX(0);
+//     height: 2px;
+//     bottom: 0;
+//     left: 0;
+//     background-color: rgba(0, 0, 0, 0.8);
+//     transform-origin: bottom right;
+//     transition: transform 0.4s cubic-bezier(0.86, 0, 0.07, 1);
+//   }
+
+//   :hover::after {
+//     transform: scaleX(1);
+//     transform-origin: bottom left;
+//   }
+// `;
+
 const NavLink = styled(Link)`
-  color: black;
-  margin-left: 15px;
-  text-decoration: none;
-  display: inline-block;
-  position: relative;
+    text-decoration: none;
+    margin-right: 15px;
+    display: inline-block;
+    position: relative;
+    margin-right: 10px;
 
-  ::after {
-    content: "";
-    position: absolute;
-    width: 100%;
-    transform: scaleX(0);
-    height: 2px;
-    bottom: 0;
-    left: 0;
-    background-color: rgba(0, 0, 0, 0.8);
-    transform-origin: bottom right;
-    transition: transform 0.4s cubic-bezier(0.86, 0, 0.07, 1);
-  }
+    border: solid black 2px;
+    color: black;
+    fill: black;
+    background: white;
+    box-shadow: 4px 4px 0 black;
+    background-color: #cbc3ff;
+    
+    border-radius: 5px;
+    transition: top 0.2s ease;
+    position: relative;
+    top: 0;
+    vertical-align: middle;
+    cursor: pointer;
+    text-transform: uppercase;
+    font-family: inherit;
+    font-weight: bold;
+    margin: 0 4px;
 
-  :hover::after {
-    transform: scaleX(1);
-    transform-origin: bottom left;
-  }
+    padding-top: 3px;
+    padding-bottom: 3px;
+    padding-left: 10px;
+    padding-right: 10px;
+
+    :hover {
+        background-color: black;
+        color: #cbc3ff;
+        box-shadow: 4px 4px 0 #cbc3ff;
+    }
+
+   
 `;
 
-const GitHubLink = styled.a`
-	color: black;
-	margin-left: 15px;
-	text-decoration: none;
-	display: inline-block;
-	position: relative;
+// const GitHubLink = styled.a`
+// 	color: black;
+// 	margin-left: 15px;
+// 	text-decoration: none;
+// 	display: inline-block;
+// 	position: relative;
 
-	::after {
-		content: "";
-		position: absolute;
-		width: 100%;
-		transform: scaleX(0);
-		height: 2px;
-		bottom: 0;
-		left: 0;
-		background-color: rgba(0, 0, 0, 0.8);
-		transform-origin: bottom right;
-		transition: transform 0.4s cubic-bezier(0.86, 0, 0.07, 1);
-	}
+// 	::after {
+// 		content: "";
+// 		position: absolute;
+// 		width: 100%;
+// 		transform: scaleX(0);
+// 		height: 2px;
+// 		bottom: 0;
+// 		left: 0;
+// 		background-color: rgba(0, 0, 0, 0.8);
+// 		transform-origin: bottom right;
+// 		transition: transform 0.4s cubic-bezier(0.86, 0, 0.07, 1);
+// 	}
 
-	:hover::after {
-		transform: scaleX(1);
-		transform-origin: bottom left;
-	}
-`;
+// 	:hover::after {
+// 		transform: scaleX(1);
+// 		transform-origin: bottom left;
+// 	}
+// `;
 
 const HomeLink = styled(NavLink)`
   margin-left: 0;
@@ -80,10 +119,39 @@ const Header = ({ siteTitle }) => (
 	<SiteHeader>
 		<Content>
 			<p>
-				<HomeLink to="/">{siteTitle}</HomeLink>
-				<NavLink to="/blog">Blog</NavLink>
-				<NavLink to="/projects">Projects</NavLink>
-				<GitHubLink href="https://github.com/niklasmtj/gatsby-starter-julia">GitHub</GitHubLink>
+				<NavLink
+					to="/"
+					activeStyle={{
+						backgroundColor: 'black',
+						color: '#cbc3ff',
+						boxShadow: '4px 4px 0 #cbc3ff'
+					}}
+				>
+					{siteTitle}
+				</NavLink>
+				{/* activeStyle={{ borderBottom: '2px solid' }} */}
+				<NavLink
+					to="/blog"
+					activeStyle={{
+						backgroundColor: 'black',
+						color: '#cbc3ff',
+						boxShadow: '4px 4px 0 #cbc3ff'
+					}}
+				>
+					Blog
+				</NavLink>
+				{/* partiallyActive={true} */}
+				<NavLink
+					to="/projects"
+					activeStyle={{
+						backgroundColor: 'black',
+						color: '#cbc3ff',
+						boxShadow: '4px 4px 0 #cbc3ff'
+					}}
+				>
+					Projects
+				</NavLink>
+				<NavLink href="https://github.com/prem315">GitHub</NavLink>
 			</p>
 		</Content>
 	</SiteHeader>
