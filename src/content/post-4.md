7576f2619e9ffd542d9bd46142141cf550321f84
Prem Patel <prem.patel3105@gmail.com>
2019-10-01 17:24:29 +0530

    added fonts

---
+++ src/content/post-4.md
@@ -0,0 +1,7 @@
+---
+title: "React with Firebase"
+date: "2019-03-17"
+draft: false
+path: "/blog/react-firebase"
+---
+# H1
---
+++ src/pages/projects.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link, graphql } from 'gatsby';
+import { css } from '@emotion/core';
+import styled from '@emotion/styled';
+import SEO from '../components/seo';
+import Layout from '../components/layout';
+
+const Content = styled.div`
+	margin: 0 auto;
+	max-width: 860px;
+	padding: 1.45rem 1.0875rem;
+`;
+
+const Projects = ({ data }) => {
+	return (
+		<Layout>
+			<SEO title="Blog" />
+			<h1>Projects</h1>
+			<Content />
+		</Layout>
+	);
+};
+
+export default Projects;
