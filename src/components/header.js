82a03f779133b71c68819f08f5bd4a7749a11bd9
Prem Patel <prem.patel3105@gmail.com>
2019-10-01 20:20:44 +0530

    added css to header

--- src/components/header.js
+++ src/components/header.js
@@ -37,7 +37,7 @@ const Content = styled.div`
 
 const NavLink = styled(Link)`
     text-decoration: none;
-    margin-right: 15px;
+    margin-right: 15px !important;
     display: inline-block;
     position: relative;
     margin-right: 10px;
