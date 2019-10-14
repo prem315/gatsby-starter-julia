bcc520b687332b880fc69d44018b2f495addef64
Prem Patel <prem.patel3105@gmail.com>
2019-10-14 16:24:42 +0530

    added netlify form

--- src/pages/contactme.js
+++ src/pages/contactme.js
@@ -140,7 +140,7 @@ const ContactMe = ({ data }) => {
 					Contact <Name>Me</Name> 📇{' '}
 				</NameHeader>
 				<Description>Here are some of the projects i have worked on and prowd of.</Description>
-				<form>
+				<form method="post" name="contact" netlify netlify-honeypot="bot-field" data-netlify="true">
 					<Label>Name</Label>
 					<br />
 					<Input name="name" type="text" />
